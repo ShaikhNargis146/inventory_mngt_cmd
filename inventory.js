267ed3d859d89a126ec462207b7574544303825b
Nargis Shaikh <nargis@onewallet.co.in>
2020-07-20 14:59:04 +0530

    comments added

--- inventory.js
+++ inventory.js
@@ -1,15 +1,18 @@
 var readline = require('readline');
 var inventory = require('./inventory.json');
 var utils = require('./utils.js');
+//to read I/O from CMD
 rl = readline.createInterface(process.stdin, process.stdout);
 var i = 0;
+//for each line entered on CMD do the following
 rl.on('line', function (line) {
     let input = line.split(':')
     let inventory_available = JSON.parse(JSON.stringify(inventory));
     let passport;
-    if (input.length > 5) {
+    if (input.length > 5) {//passport available
         passport = input.splice(1, 1)[0];
     }
+    //calculate the price and left over inventory and is generic for n number of products(if its added in inventory)
     const output = getPriceWithinventory_left(inventory_available, input.splice(0, 1)[0], input, passport);
     console.log(
         `OUTPUT ${i}: ${output}`
@@ -36,27 +39,24 @@ const getPriceWithinventory_left = (inventory_available, country, input, passpor
             inventory_available = JSON.parse(JSON.stringify(inventory));
             return `OUT_OF_STOCK:${inventory_available["UK"]["Mask"].quantity}:${inventory_available["Germany"]["Mask"].quantity}:${inventory_available["UK"]["Gloves"].quantity}:${inventory_available["Germany"]["Gloves"].quantity}`;
         } else {
-            let isCheaperPriceAvailable = utils.isCheaperPriceAvailable(country, otherCountry, product, passport, inventory_available);
-            if (isCheaperPriceAvailable) {
+            if (utils.isCheaperPriceAvailable(country, otherCountry, product, passport, inventory_available)) {//if cheaper price available from other country
                 const cheaperQty = qty - (qty % 10);
                 const costlyQty = qty % 10;
-                const transportCharges = utils.getTansportaionCharge(cheaperQty, isLocalPassport)
                 total_bill +=
-                    inventory_available[otherCountry][product].price * cheaperQty + transportCharges;
+                    inventory_available[otherCountry][product].price * cheaperQty + utils.getTansportaionCharge(cheaperQty, isLocalPassport);
                 total_bill += inventory_available[country][product].price * costlyQty;
                 inventory_available[country][product].quantity -= costlyQty;
                 inventory_available[otherCountry][product].quantity -= cheaperQty;
-            } else if (inventory_available[country][product].quantity >= qty) {
+            } else if (inventory_available[country][product].quantity >= qty) {//if inventory is sufficient in this country and its cheapest
                 total_bill += inventory_available[country][product].price * qty;
                 inventory_available[country][product].quantity -= qty;
             } else if (
                 inventory_available[country][product].quantity + inventory_available[otherCountry][product].quantity >= qty
-            ) {
+            ) {// if quantity is greater than th current country inventory and product needs to be imported
                 const transportQty = qty - inventory_available[country][product].quantity;
-                const transportationCharges = utils.getTansportaionCharge(transportQty, isLocalPassport)
                 total_bill +=
                     inventory_available[country][product].price * inventory_available[country][product].quantity +
-                    inventory_available[otherCountry][product].price * transportQty + transportationCharges;
+                    inventory_available[otherCountry][product].price * transportQty + utils.getTansportaionCharge(transportQty, isLocalPassport);
                 inventory_available[country][product].quantity = 0;
                 inventory_available[otherCountry][product].quantity -= transportQty;
             }
--- utils.js
+++ utils.js
@@ -1,12 +1,15 @@
 class Utils {
+    //check if passport is local wrt the given country
     static isLocalPassport(country, passport) {
         return passport &&
             ((passport[0] === "B" && !isNaN(passport.substring(1, 4)) && /^[A-Z]+$/.test(passport.substring(4, 6)) && /^[A-Z0-9]+$/.test(passport.substring(6)) && country === "UK") ||
                 (passport[0] === "A" && /^[A-Z]+$/.test(passport.substring(1, 3)) && /^[A-Z0-9]+$/.test(passport.substring(3)) && country === "Germany"))
     }
+    //get the transportation cost if its local or foriegn
     static getTansportaionCharge(qty, isLocal) {
         return Math.ceil(qty / 10) * ((isLocal && 320) || 400);
     }
+    //check if cheaper price available from other country
     static isCheaperPriceAvailable = (country, otherCountry, item, passport, inventory) => {
         var cheapPriceCountry = inventory[country][item].price > inventory[otherCountry][item].price ? otherCountry : country;
         let isLocal = this.isLocalPassport(cheapPriceCountry, passport)
